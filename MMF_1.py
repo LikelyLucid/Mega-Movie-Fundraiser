39aea16d615e323736247153ee32ec687f0ba6ea
PurelyStupidity <micoolplays@gmail.com>
2022-03-15 14:24:37 +1300

    March 15, 2022 2:24 PM

--- MMF_1.py
+++ MMF_1.py
@@ -2,7 +2,4 @@ def check_blank(question):
     valid = ""
     while not valid:
         valid = input(question)
-        if valid.strip():
-            return valid
-        else:
-            print("Please enter a value.")
+        
