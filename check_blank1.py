f4e8e0e9a5279b52cfe9f82fe1230bba9385e03f
PurelyStupidity <micoolplays@gmail.com>
2022-03-17 11:56:51 +1300

    March 17, 2022 11:56 AM

--- check_blank1.py
+++ check_blank1.py
@@ -2,6 +2,6 @@ def check_blank(question):
     while True: # repeat forever
         response =  input(question)
         if response == "": # if response is empty
-            print("You must enter something!")
+            print("You must enter something!") # print error message
         else:
             return response # exit the function and return the response
