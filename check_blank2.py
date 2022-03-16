5464b07b507d7e8b47ae9b1bcc03320a65dbeb1b
PurelyStupidity <micoolplays@gmail.com>
2022-03-17 11:46:09 +1300

    March 17, 2022 11:46 AM

--- check_blank2.py
+++ check_blank2.py
@@ -1,7 +1,7 @@
 def check_blank(question, error_msg):
     while True:
         response =  input(question)
-        if response == "":
+        if response
             print(error_msg)
         else:
             return response
