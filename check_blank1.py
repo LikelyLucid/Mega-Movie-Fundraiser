f953acf770450a771b042bd16ff0d1c0f4ad689c
PurelyStupidity <micoolplays@gmail.com>
2022-03-17 14:08:00 +1300

    March 17, 2022 2:08 PM

--- check_blank1.py
+++ check_blank1.py
@@ -4,4 +4,4 @@ def check_blank(question):
         if response == "": # if response is empty
             print("You must enter something!") # print error message
         else:
-            return response # exit the function and return the response
+            return response  # exit the function and return the response
