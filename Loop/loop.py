e2ddf0f49ffef15281b7082fe4bca0c5ab2d9cf1
PurelyStupidity <micoolplays@gmail.com>
2022-03-29 11:04:27 +1300

    March 29, 2022 11:04 AM

--- Loop/loop.py
+++ Loop/loop.py
@@ -4,4 +4,4 @@ MAX_TICKETS = 5 # max amount of tickets
 
 while name != "Xxx" and count != MAX_TICKETS: # while loop
     name = input("What's your name? ").title() # input name
-    count += 1
+    count += 1 # count + 1
