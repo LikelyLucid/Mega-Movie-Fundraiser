ababae5618684f2108fb9c8dc8ae3373754a2789
PurelyStupidity <micoolplays@gmail.com>
2022-03-29 14:42:50 +1300

    March 29, 2022 2:42 PM

---
+++ 03_age_validater/age_vaildater_v2.py
@@ -0,0 +1,14 @@
+def interger_checker(question, low_num, high_num):
+    error = f"Please enter an integer between {low_num} and {high_num}."
+    valid = False
+    while not valid:
+        try:
+            number_to_check = int(input(question))
+            if low_num <= number_to_check <= high_num:
+                return number_to_check
+            else:
+                print(error)
+        except ValueError:
+            print("\nPlease enter an integer (eg a whole number with no decimal)")
+age = interger_checker("\nPlease enter age of ticker holder: ", 12, 110)
+print(f"Age = {age}")
