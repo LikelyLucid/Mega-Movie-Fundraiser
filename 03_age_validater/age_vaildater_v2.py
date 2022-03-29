188a444f7f8056456d428a1b31819b3c83b2c835
PurelyStupidity <micoolplays@gmail.com>
2022-03-29 14:43:56 +1300

    March 29, 2022 2:43 PM

--- 03_age_validater/age_vaildater_v2.py
+++ 03_age_validater/age_vaildater_v2.py
@@ -3,7 +3,8 @@ def number_checker(text):
     while not valid:
         try:
             number_to_check = int(input(question))
-            if isinstance()
+            if isinstance(number_to_check, int):
+                return number_to_check
         except ValueError:
             print("\nPlease enter an integer (eg a whole number with no decimal)")
 age = interger_checker("\nPlease enter age of ticker holder: ", 12, 110)
