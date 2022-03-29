6ac12604e3986539b052b16dc359340d52a1b980
PurelyStupidity <micoolplays@gmail.com>
2022-03-29 14:13:09 +1300

    March 29, 2022 2:13 PM

--- 03_age_validater/age_vaildater_v1.py
+++ 03_age_validater/age_vaildater_v1.py
@@ -2,4 +2,5 @@ def interger_checker(question, low_num, high_num):
     error = f"Please enter an integer between {low_num} and {high_num}."
     valid = False
     while not valid:
-        
+        try:
+            
