b0f56b3b67742c479d381fe17b5e65330a361ee2
PurelyStupidity <micoolplays@gmail.com>
2022-03-29 13:51:00 +1300

    March 29, 2022 1:51 PM

--- Loop/loop3.py
+++ Loop/loop3.py
@@ -3,7 +3,7 @@ count = 0
 MAX_TICKETS = 5  # max amount of tickets
 
 while name != "Xxx" and count != MAX_TICKETS:
-    if MAX_TICKETS - count > 1: # if there are more than 1 tickets left
+    if MAX_TICKETS - count > 1:  # if there are more than 1 tickets left
         print(f"\nYou have {MAX_TICKETS - count} ticket left")
     else:
         # if there is only 1 ticket left
@@ -13,6 +13,8 @@ while name != "Xxx" and count != MAX_TICKETS:
         count += 1
 
 if count < MAX_TICKETS:
-    print(f"\nYou have sold {count} tickets\nThere are {MAX_TICKETS - count} tickets left")
+    print(
+        f"\nYou have sold {count} tickets\nThere are {MAX_TICKETS - count} tickets left"
+    )
 else:
     print("\nYou have sold all available tickets")
