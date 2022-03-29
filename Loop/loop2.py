38c6d837085c1ba39977e4fb8eb4f93000ddbaba
PurelyStupidity <micoolplays@gmail.com>
2022-03-29 13:43:36 +1300

    March 29, 2022 1:43 PM

--- Loop/loop2.py
+++ Loop/loop2.py
@@ -10,7 +10,7 @@ while name != "Xxx" and count != MAX_TICKETS:
         print("****YOU ONLY HAVE ONE TICKET LEFT*****")
     name = input("What's your name? ").title()
     if name != "Xxx":
-    count += 1
+        count += 1
 
 if count < MAX_TICKETS:
     print(f"You have sold {count} tickets\nThere are {MAX_TICKETS - count} tickets left")
