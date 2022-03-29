e3d4e3f2e077126f331f9eac1e7101f5433089d4
PurelyStupidity <micoolplays@gmail.com>
2022-03-29 14:15:43 +1300

    March 29, 2022 2:15 PM

--- 03_age_validater/age_vaildater_v1.py
+++ 03_age_validater/age_vaildater_v1.py
@@ -10,4 +10,4 @@ def interger_checker(question, low_num, high_num):
                 print(error)
         except ValueError:
             print("\nPlease enter an integer (eg a whole number with no decimal")
-age = interger_checker("\nPlease enter age of ticker holder: ", 0, 150)
+age = interger_checker("\nPlease enter age of ticker holder: ", 12, 150)
