9c153a16c0e4d7f64b07e5a360efed89075f29f5
PurelyStupidity <micoolplays@gmail.com>
2022-03-30 18:47:39 +1300

    March 30, 2022 6:47 PM

--- 05_yes_no/yes_n0_v2.py
+++ 05_yes_no/yes_n0_v2.py
@@ -7,5 +7,6 @@ def yes_no_response(question):
         response = input(question).lower()
     if response == "n" or response == "no":
         return False
-    else: return True
+    else:
+        return True
 .# Main routine # temporary input statements - during development snacks_required = yes_no_response("Do you want snacks?") if not snacks_required: print("Valid answer. You don't want snacks") else: print("Valid answer. You do want snacks")
