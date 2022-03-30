b357534af4c823a90e671f82680015689732841e
PurelyStupidity <micoolplays@gmail.com>
2022-03-30 19:38:54 +1300

    March 30, 2022 7:38 PM

--- 06_String_validator/string_validator_v3.py
+++ 06_String_validator/string_validator_v3.py
@@ -1,6 +1,5 @@
 def get_choice(question, valid_choices):
     choice_error = "Sorry, that is not a valid choice"
-
     choice = input(question).lower()
     for item in valid_choices:
         if choice in item:
@@ -11,9 +10,24 @@ def get_choice(question, valid_choices):
     return get_choice(question, valid_choices)
 
 
-ask_for_snack = "What snack do you want: "
+ask_for_snack = "What snack do you want - 'x' to stop ordering: "
 valid_snacks = [["popcorn", "p", "corn", "1"], ["m&ms", "mms", "m", "2"],
                     ["pita chips", "chips", "pc", "pita", "c", "3"],
-                    ["water", "w", "4"]]
-for test in range(6):
-    print(f"You have chosen {get_choice(ask_for_snack, valid_snacks)}")
+                    ["water", "w", "4"], ["x", "X", "exit", "5"]]
+
+check_snacks = "Do you want snacks? "
+valid_yes_no = [["y", "yes"], ["n", "no"]]
+
+getting_snacks = True
+snacks_required = get_choice(check_snacks, valid_yes_no)
+while getting_snacks:
+    if snacks_required == "N":
+        print("You don't want snacks")
+        getting_snacks = False
+    else:
+        option = get_choice(ask_for_snack, valid_snacks)
+        if option != "X":
+            print(f"You have chosen {option}")
+        else:
+            getting_snacks = False
+            print("Thanks for ordering your snacks")
