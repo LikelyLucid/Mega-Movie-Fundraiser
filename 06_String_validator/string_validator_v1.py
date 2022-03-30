edeb2be008222ad8837e39ca5adff7218b607a5a
PurelyStupidity <micoolplays@gmail.com>
2022-03-30 19:22:59 +1300

    March 30, 2022 7:22 PM

--- 06_String_validator/string_validator_v1.py
+++ 06_String_validator/string_validator_v1.py
@@ -2,6 +2,7 @@ def get_snacks():
     snack_choice_error = "Sorry, that is not a valid choice"
     valid_snacks = [["popcorn", "p", "corn", "1"], ["m&ms", "mms", "m", "2"], ["pita chips", "chips", "pc", "pita", "c", "3"], ["water", "w", "4"]] snack_choice = input("Snack: ").lower()
     for snack in valid_snacks:
-        if snack_choice in snack: snack_choice = snack[0].title() return snack_choice
+        if snack_choice in snack:
+            snack_choice = snack[0].title() return snack_choice
 print(snack_choice_error) return get_snacks()
 4 Main routine 4 temporary input statement - during development for test in range(6): print(f"You want {get_snacks()}")
