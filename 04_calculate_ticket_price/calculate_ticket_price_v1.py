87475dd3bf88a730174bc1247d6497978576603e
PurelyStupidity <micoolplays@gmail.com>
2022-03-30 17:52:34 +1300

    March 30, 2022 5:52 PM

--- 04_calculate_ticket_price/calculate_ticket_price_v1.py
+++ 04_calculate_ticket_price/calculate_ticket_price_v1.py
@@ -2,15 +2,13 @@ def calculate_ticket_price(age):
     CHILD_AGE = range(12, 16)
     STANDARD_AGE = range(16, 65)
 
-    CHILD_PRICE = 7.5
     STANDARD_PRICE = 10.5
-    RETIRED_PRICE = 6.5
     if age in CHILD_AGE:
-        ticket_price = CHILD_PRICE
+        return 7.5
     elif age in STANDARD_AGE:
-        ticket_price = STANDARD_PRICE
-    else: ticket_price = RETIRED_PRICE
-    return(ticket_price)
+        return STANDARD_PRICE
+    else:
+        return 6.5
 
 
 name = input("Name: ")
