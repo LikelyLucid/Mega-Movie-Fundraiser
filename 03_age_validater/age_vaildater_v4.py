857144e7f75d62d31a827b6b3b483c951e347d44
PurelyStupidity <micoolplays@gmail.com>
2022-03-30 17:30:24 +1300

    March 30, 2022 5:30 PM

--- 03_age_validater/age_vaildater_v4.py
+++ 03_age_validater/age_vaildater_v4.py
@@ -13,4 +13,4 @@ def number_checker(text):
             print("\nPlease enter an integer (eg a whole number with no decimal)")
 
 age = number_checker("\nPlease enter age of ticker holder: ")
-if age < MIN_AFE
+if age < MIN_AGE
