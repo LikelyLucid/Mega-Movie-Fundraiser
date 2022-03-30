82671a9efd6f56d79259be8bcdd6a6474bbcd528
PurelyStupidity <micoolplays@gmail.com>
2022-03-30 17:50:17 +1300

    March 30, 2022 5:50 PM

--- 04_calculate_ticket_price/calculate_ticket_price_v1.py
+++ 04_calculate_ticket_price/calculate_ticket_price_v1.py
@@ -2,8 +2,9 @@ def calculate_ticket_price(age):
 CHILD_AGE = range(12, 16)
 STANDARD_AGE = range(16, 65)
 CHILD_PRICE = 7.5
-STANDARD_PRICE = 10.5 
+STANDARD_PRICE = 10.5
 RETIRED_PRICE = 6.5
-if age in CHILD_AGE: ticket_price = CHILD_PRICE elif age in STANDARD_AGE: ticket_price = STANDARD_PRICE else: ticket_price = RETIRED_PRICE return(ticket_price)
+if age in CHILD_AGE:
+    ticket_price = CHILD_PRICE elif age in STANDARD_AGE: ticket_price = STANDARD_PRICE else: ticket_price = RETIRED_PRICE return(ticket_price)
 # Main routine # temporary input statements - during development name = input("Name: ") age = int(input("Age: "))
 print(f"The price is $icalculate_ticket_price(age):,.2f1")
