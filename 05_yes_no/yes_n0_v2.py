7355025cb0e5e888e4ae8792c9b51b194fcdcbf8
PurelyStupidity <micoolplays@gmail.com>
2022-03-30 18:46:47 +1300

    March 30, 2022 6:46 PM

---
+++ 05_yes_no/yes_n0_v2.py
@@ -0,0 +1,13 @@
+error_message = "Please answer 'Y' or 'N'"
+valid_responses = ["y", "yes", "n", "no"]
+response = input("Do you want snacks? ").lower()
+
+while response not in valid_responses:
+    print(error_message)
+    response = input("Do you want snacks? ").lower()
+
+    if response == "n" or response == "no":
+        print("Valid answer. You don't want snacks")
+
+    else:
+        print("Valid answer. You do want snacks")
