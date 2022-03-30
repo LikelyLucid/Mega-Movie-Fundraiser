937e2c07a107dd5d526b7d02f98da867165b2e24
PurelyStupidity <micoolplays@gmail.com>
2022-03-30 18:04:12 +1300

    March 30, 2022 6:04 PM

--- 04_calculate_ticket_price/calculate_ticket_price_v4.py
+++ 04_calculate_ticket_price/calculate_ticket_price_v4.py
@@ -22,4 +22,4 @@ for test in test_cases:
     test_age = test[1]
     ticket_price = calculate_ticket_price(test_age)
     print(f"For {test_name} the price is ${ticket_price:,.2f}")
-    profit
+    profit += ticket_price - TICKET_COST_PRICE
