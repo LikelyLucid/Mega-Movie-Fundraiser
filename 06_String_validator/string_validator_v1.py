071b63d715b614ef4116b541d3d7d689a8e0b5f6
PurelyStupidity <micoolplays@gmail.com>
2022-03-30 19:33:51 +1300

    March 30, 2022 7:33 PM

--- 06_String_validator/string_validator_v1.py
+++ 06_String_validator/string_validator_v1.py
@@ -1,6 +1,6 @@
 def get_snacks():
     snack_choice_error = "Sorry, that is not a valid choice"
-    valid_snacks = [["popcorn", "p", "corn", "1"], ["m&ms", "mms", "m", "2"]["pita chips", "chips", "pc", "pita", "c", "3"], ["water", "w", "4"]]
+    valid_snacks = [["popcorn", "p", "corn", "1"], ["m&ms", "mms", "m", "2"],["pita chips", "chips", "pc", "pita", "c", "3"], ["water", "w", "4"]]
     snack_choice = input("Snack: ").lower()
     for snack in valid_snacks:
         if snack_choice in snack:
