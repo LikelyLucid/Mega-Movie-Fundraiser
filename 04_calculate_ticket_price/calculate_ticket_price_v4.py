971026446277333a0273571d4f034bdedee654e9
PurelyStupidity <micoolplays@gmail.com>
2022-03-30 18:02:55 +1300

    March 30, 2022 6:02 PM

--- 04_calculate_ticket_price/calculate_ticket_price_v4.py
+++ 04_calculate_ticket_price/calculate_ticket_price_v4.py
@@ -14,7 +14,7 @@ def calculate_ticket_price(age):
         ticket_price = retired_price
     return ticket_price
 
-
+TICKET
 test_cases = [["Rangi", 15], ["Manaia", 16], ["Talia", 64], ["Arihi", 65]]
 
 for test in test_cases:
