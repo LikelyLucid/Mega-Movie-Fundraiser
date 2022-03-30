b170c7b1e0e41aef11677e4db7ba6eef8f59c1a6
PurelyStupidity <micoolplays@gmail.com>
2022-03-30 17:30:39 +1300

    March 30, 2022 5:30 PM

--- 03_age_validater/age_vaildater_v4.py
+++ 03_age_validater/age_vaildater_v4.py
@@ -15,5 +15,3 @@ def number_checker(text):
 age = number_checker("\nPlease enter age of ticker holder: ")
 if age < MIN_AGE:
     print(f"\nSorry, you must be at least {MIN_AGE} years old to hold the stock.")
-
-    
