ff8c3c79c385d112d8cf6f68bfd80c23bace1c07
PurelyStupidity <micoolplays@gmail.com>
2022-03-30 18:00:24 +1300

    March 30, 2022 6:00 PM

--- 04_calculate_ticket_price/calculate_ticket_price_v3.py
+++ 04_calculate_ticket_price/calculate_ticket_price_v3.py
@@ -16,5 +16,6 @@ def calculate_ticket_price(age):
 
 
 test_cases = [["Rangi", 15],["Manaia", 16], ["Talia", 64], ["Arihi", 65]
-              
-for test in test_cases: test_name = test[0] test_age = test[1] print(f"For {test_name} the price is $icalculate_ticket_price(test_age):,.2f1")
+
+for test in test_cases:
+    test_name = test[0] test_age = test[1] print(f"For {test_name} the price is $icalculate_ticket_price(test_age):,.2f1")
