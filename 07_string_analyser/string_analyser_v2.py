b3288379d7ddf2128418f10cf4d8bcdecc05b245
PurelyStupidity <micoolplays@gmail.com>
2022-04-05 13:57:33 +1200

    April 5, 2022 1:57 PM

--- 07_string_analyser/string_analyser_v2.py
+++ 07_string_analyser/string_analyser_v2.py
@@ -1,4 +1,21 @@
-my_string = "Hello world"
+test_strings = [
+    "Popcorn",
+    "2 pc",
+    "1.5OJ",
+    "4OJ",
+    "120Chips",
+    ]
 
-print(f"First letter: {my_string[0]}")
-print(f"Rest of string: {my_string[1:]}")
+for item in test_strings:
+    if item[0].isdigit():
+        if item[1].isdigit():
+            quantity = int(item[0]+item[1])
+            snack = item[2:]
+        else:
+            quantity = int(item[0])
+            snack = item[1:]
+    else:
+        quantity = 1
+        snack = item
+    print(f"Quantity = {quantity}")
+    print(f"Snack = {snack}")
