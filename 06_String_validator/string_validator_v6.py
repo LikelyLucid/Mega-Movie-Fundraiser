1604c9024290dfc69aa5e84a230288ce6a7cab31
PurelyStupidity <micoolplays@gmail.com>
2022-04-05 14:10:54 +1200

    April 5, 2022 2:10 PM

--- 06_String_validator/string_validator_v6.py
+++ 06_String_validator/string_validator_v6.py
@@ -21,44 +21,47 @@ def get_choice(choice, valid_choices):
             return choice
     print(choice_error)
 
+def collate_order():
+    valid_snacks = [["popcorn", "p", "corn", "(1"], ["m&ms", "mms", "m", "(2"],
+                    ["pita chips", "chips", "pc", "pita", "c", "(3"],
+                    ["water", "w", "(4"], ["orange juice", "oj", "(5"],
+                    ["x", "X", "exit", "(6"]]
+    valid_yes_no = [["y", "yes"], ["n", "no"]]
+    snack_order = []
+    max_number_of_snacks = 4
+    getting_snacks = True
 
-valid_snacks = [
-    ["popcorn", "p", "corn", "(1"],
-    ["m&ms", "mms", "m", "(2"],
-    ["pita chips", "chips", "pc", "pita", "c", "(3"],
-    ["water", "w", "(4"],
-    ["orange juice", "oj", "(5"],
-    ["x", "X", "exit", "(6"],
-]
-valid_yes_no = [["y", "yes"], ["n", "no"]]
-snack_order = []
-max_number_of_snacks = 4
-getting_snacks = True
+    while getting_snacks:
+        snacks_required = ""
+        while snacks_required != "N" and snacks_required != "Y":
+            check_snacks = input("Do you want snacks? (Y/N): ").lower()
+            snacks_required = get_choice(check_snacks, valid_yes_no)
+        if snacks_required == "N":
+            getting_snacks = False
+            break
+        else:
+            option = ""
+            while option != "X":
+                snack = input(
+                    "What snack do you want - or 'X' to stop ordering: "
+                    ).lower()
+                snack = split_order(snack)
+                quantity = snack[0]
+                if quantity > max_number_of_snacks:
+                    snack = None
+                    print("Sorry, the maximum number you can order is 4")
+                else:
+                    snack = snack[1]
+                    option = get_choice(snack, valid_snacks)
+                    if option == "X":
+                        getting_snacks = False
+                    elif option is not None:
+                        snack_order.append([quantity, option])
+    return snack_order
 
-while getting_snacks:
-    snacks_required = ""
-    while snacks_required != "N" and snacks_required != "Y":
-        check_snacks = input("Do you want snacks? (Y/N): ").lower()
-        snacks_required = get_choice(check_snacks, valid_yes_no)
-    if snacks_required == "N":
-        getting_snacks = False
-        break
-    else:
-        option = ""
-        while option != "X":
-            snack = input("What snack do you want - or 'X' to stop ordering: ").lower()
-            snack = split_order(snack)
-            quantity = snack[0]
-            if quantity > max_number_of_snacks:
-                snack = None
-                print("Sorry, the maximum number you can order is 4")
-            else:
-                snack = snack[1]
-                option = get_choice(snack, valid_snacks)
-                if option == "X":
-                    getting_snacks = False
-                elif option is not None:
-                    snack_order.append([quantity, option])
+
+snack_order = collate_order()
+
 
 if len(snack_order) > 0:
     print("\nThis is a summary of your order: ")
