b211511126ea07df494a3e303cb0ff0572736d3a
PurelyStupidity <micoolplays@gmail.com>
2022-04-19 21:29:09 +1200

    April 19, 2022 9:29 PM

--- initialise_snack_lists_V2.py
+++ initialise_snack_lists_V2.py
@@ -1,3 +1,5 @@
+import pandas
+
 names = ["Rangi", "Manaia", "Talia", "Arihi", "Fetu"]
 
 popcorn = []
@@ -6,7 +8,8 @@ pita_chips = []
 water = []
 orange_juice = []
 snack_lists = [popcorn, mms, pita_chips, water, orange_juice]
-snack_menu_dict = {
+movie_data_dict = {
+    "Name": names,
     "Popcorn": popcorn,
     "Water": water,
     "Pita Chips": pita_chips,
@@ -33,12 +36,17 @@ for client_order in test_data:
         if len(item) > 0:
             to_find = item[1]
             amount = item[0]
-            add_list = snack_menu_dict[to_find]
+            add_list = movie_data_dict[to_find]
             add_list[-1] = amount
-
 
 print(f"Popcorn: {snack_lists[0]}")
 print(f"M&Ms: {snack_lists[1]}")
 print(f"Pita Chips: {snack_lists[2]}")
 print(f"Water: {snack_lists[3]}")
 print(f"Orange Juice: {snack_lists[4]}")
+print()
+
+
+movie_frame = pandas.DataFrame(movie_data_dict)
+movie_frame = movie_frame.set_index("Name")
+print(movie_frame)
